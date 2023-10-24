12ea9245cb462937b7dec9ec1d9c4c050b0db484
LSaldiasV <128053880+LSaldiasV@users.noreply.github.com>
2023-10-24 00:07:23 -0300

    Add files via upload
    hice hasta el video 4

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,28 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+
+  plugins: {
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: false,
+      iosKeychainPrefix: 'YOUR_APP_NAME',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: false,
+      androidBiometric: {
+        biometricAuth : false,
+        biometricTitle : "Biometric login for capacitor sqlite",
+        biometricSubTitle : "log in using your biometric"
+      },
+      electronIsEncryption: false,
+      electronWindowsLocation: "C:\\PogramData\\CapacitorDatabases",
+      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+      electronLinuxLocation: "Databases"
+    }
   }
 };
 
